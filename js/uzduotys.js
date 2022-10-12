e3344e89d13ea883c1dac899c00a692af80eb48e
EyeMLeo <Leonid.Akuta@gmail.com>
2022-10-12 13:16:39 +0200

    Phone user yes or no

--- js/uzduotys.js
+++ js/uzduotys.js
@@ -59,10 +59,35 @@ if (nameLong.length>=5) {
 
 // Sukurkite du kintamuosius: 'clientAge' ir 'legalAge'. Patikriname ar vartotojas gali vairuoti automobilį - t.y. clientAge didesnis arba lygus legalAge. Jei taip - išmeta 'Can Drive', kitaip 'Can't drive'.
 
+let clientAge=5;
 
+let legalAge=18;
+
+if (clientAge>=legalAge) {
+    console.log("can drive");
+} else{
+    console.log("not allowed to drive");
+
+}
 
 
 // Sukurkite variable 'phone', kuris būtų lygus 'iPhone'. Taip pat sukurkite variable isIphoneUser, kuris turėtų būti prilygintas Boolean reikšmei (t.y. true arba false) priklausomai ar phone yra 'iPhone' ar bet koks kitas.
+let phone = "nokia".toLowerCase();
+console.log(phone);
+
+
+// phone==="iphone" ? console.log("apple vartotojas"): console.log("kitas vartotojas")
+
+let isIphoneUser;
+if ( phone ==="iphone"|| phone ==="android"||phone ==="samsung"||phone ==="nokia"||phone ==="siemens") {
+    isIphoneUser = true;
+} else {
+    isIphoneUser = false;
+}
+console.log(`is phone user ${isIphoneUser}`);
+
+// let isIphoneUser= true;
+
 
 // Sukurk variable 'car', kuris bus lygus kažkokiam automobilio prekės ženklui. Sukurk if-else (arba switch), kuris pasakys ar tas brand'as priklauso VW Group, ar BMW Group, ar nei vienam. VW group priklauso: VW, Audi, Bentley, Bugatti, Lamborghini, Porsche. BMW group priklauso: BMW, Mini, Rolls-Royce.
 
