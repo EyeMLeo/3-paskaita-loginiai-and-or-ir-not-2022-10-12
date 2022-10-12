d1708ab1aadbb3d9c36bd0284e511218dd18f3aa
EyeMLeo <Leonid.Akuta@gmail.com>
2022-10-12 12:34:18 +0200

    long or short name

--- js/uzduotys.js
+++ js/uzduotys.js
@@ -41,3 +41,28 @@ let numberRemain=(number2/2) % 1;
     console.log("nelyginis skaicius");
  }
 
+
+
+
+// Parašykite variable su jūsų vardu. Pasiklauskite, jei vardo ilgis trumpesnis nei 5 simboliai - tegul išmeta konsolėje "Short Name", kitaip - "Long Name".
+
+let nameLong = "as"
+console.log(`${nameLong.length} nameLong.length`)
+
+if (nameLong.length>=5) {
+    console.log("Ilgas vardas");
+    
+}else{
+    console.log("trumpas vardas");
+}
+
+
+// Sukurkite du kintamuosius: 'clientAge' ir 'legalAge'. Patikriname ar vartotojas gali vairuoti automobilį - t.y. clientAge didesnis arba lygus legalAge. Jei taip - išmeta 'Can Drive', kitaip 'Can't drive'.
+
+
+
+
+// Sukurkite variable 'phone', kuris būtų lygus 'iPhone'. Taip pat sukurkite variable isIphoneUser, kuris turėtų būti prilygintas Boolean reikšmei (t.y. true arba false) priklausomai ar phone yra 'iPhone' ar bet koks kitas.
+
+// Sukurk variable 'car', kuris bus lygus kažkokiam automobilio prekės ženklui. Sukurk if-else (arba switch), kuris pasakys ar tas brand'as priklauso VW Group, ar BMW Group, ar nei vienam. VW group priklauso: VW, Audi, Bentley, Bugatti, Lamborghini, Porsche. BMW group priklauso: BMW, Mini, Rolls-Royce.
+
