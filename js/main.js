dd5568fd6cb62d88a48d9ba8e1001dcbad645ee0
EyeMLeo <Leonid.Akuta@gmail.com>
2022-10-12 09:49:16 +0200

    dienoskymas

--- js/main.js
+++ js/main.js
@@ -9,14 +9,53 @@ console.log('laikas.js');
  
 */
 
-let laikas = 0;
+let laikas = 24;
 
 if (laikas >= 6 && laikas <= 12) {
   console.log('labas rytas');
 } else if (laikas >= 13 && laikas <= 18) {
   console.log('laba diena');
-} else if (laikas >= 19 && laikas <= 23) {
+} else if (laikas >= 19 && laikas < 24) {
     console.log('labas vakaras');
-}else if (laikas >= 0 && laikas <= 5) {
+}else if (laikas >= 0 && laikas <= 5 || laikas ===24) {
   console.log('labos nakties');
 }
+
+
+
+
+
+// DIENOS BANDYMAI
+
+let dienadienis = +(Math.random()*10*0.6+1).toFixed(0)
+console.log(dienadienis);
+
+switch(dienadienis){
+case 1:
+    console.log("pirmadienis");
+    break;
+case 2:
+    console.log("antradienis");
+    break;
+case 3:
+    console.log("treciadienis");
+    break;
+case 4:
+    console.log("ketvirtadienis");
+    break;
+case 5:
+    console.log("penktadienis");
+    break;
+case 6:
+    console.log("sestadienis");
+    break;
+case 7:
+    console.log("sekmadienis");
+    break;
+default:
+    console.log("Iviko klaida");
+
+}
+
+
+// 
